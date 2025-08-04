5d5a4beccd8c06ea1b5dd5225a1d2e6cb89a4bd3
fantasy-zrq <384130598@qq.com>
2025-08-04 15:47:32 +0800

    init-commit

---
+++ src/main/java/com/example/myspb/MyspbApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyspbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myspb/MyspbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
